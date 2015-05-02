0eb3f7452455e1c4fcd835f7d888f5bb644d21b7
Andres Montano <amontano@virginia.edu>
2015-05-02 00:56:22 -0600

    updating dictionary url

--- app/models/dictionary_resource.rb
+++ app/models/dictionary_resource.rb
@@ -5,7 +5,7 @@ class DictionaryResource < ActiveResource::Base
     headers['Host'] = 'dev.dictionary.thlib.org'
   when InterfaceUtils::Server::STAGING
     self.site = 'http://127.0.0.1/' 
-    headers['Host'] = 'staging.dictionary.thlib.org'
+    headers['Host'] = 'staging-dictionary.thlib.org'
   when InterfaceUtils::Server::PRODUCTION
     self.site = 'http://127.0.0.1/'
     headers['Host'] = 'dictionary.thlib.org' 
--- lib/dictionary_integration/version.rb
+++ lib/dictionary_integration/version.rb
@@ -1,3 +1,3 @@
 module DictionaryIntegration
-  VERSION = '2.1.2'
+  VERSION = '2.1.3'
 end
