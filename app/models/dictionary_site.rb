c5b1fdacdbf85ed19e095c076e94616a71e92b0b
Andres Montano <amontano@virginia.edu>
2025-07-11 10:20:32 -0600

    pointing to terms now

--- app/models/dictionary_site.rb
+++ app/models/dictionary_site.rb
@@ -4,9 +4,9 @@ class DictionarySite
   def self.get_url
     Rails.cache.fetch('dictionary/domain', :expires_in => 1.day) do
       case InterfaceUtils::Server.environment
-      when InterfaceUtils::Server::DEVELOPMENT                                                              then 'http://dev.dictionary.thlib.org/'
-      when InterfaceUtils::Server::STAGING                                                                  then 'http://staging.dictionary.thlib.org/'
-      when InterfaceUtils::Server::PRODUCTION, InterfaceUtils::Server::LOCAL, InterfaceUtils::Server::OTHER then 'http://dictionary.thlib.org/'
+      when InterfaceUtils::Server::DEVELOPMENT                                                              then 'http://dev-terms.kmaps.virginia.edu/'
+      when InterfaceUtils::Server::STAGING                                                                  then 'http://staging-terms.kmaps.virginia.edu/'
+      when InterfaceUtils::Server::PRODUCTION, InterfaceUtils::Server::LOCAL, InterfaceUtils::Server::OTHER then 'http://terms.kmaps.virginia.edu/'
       end
     end
   end
--- lib/dictionary_integration/version.rb
+++ lib/dictionary_integration/version.rb
@@ -1,3 +1,3 @@
 module DictionaryIntegration
-  VERSION = '2.1.4'
+  VERSION = '2.1.5'
 end
